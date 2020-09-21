fc8a117ce8049b0df368aec0db3b1ed38bb2650e
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-21 16:22:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,19 @@
 # Your code goes here!
 
 class Dog 
-
+ def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
+   
+   def bark=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
 end 
